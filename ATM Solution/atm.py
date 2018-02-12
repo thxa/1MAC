11fa9a91edb104742b5f12b3e4d28e6b348eba72
0xThamer <thamertx0@gmail.com>
2018-02-12 22:05:55 +0300

    r

--- ATM Solution/atm.py
+++ ATM Solution/atm.py
@@ -1,33 +1,32 @@
 def atm(balance , request):
 # allowed papers: 100, 50, 10, 5, and rest of request
-    while request > 0:
-        g = "give "
+    if balance < request:
+        print "none"
 
-        if balance < request:
-            print "none"
-            break
+    else:
+        while request > 0:
+            g = "give "
+            if request >= 100:
+                request -= 100
+                print g + str(100)
 
-        if request >= 100:
-            request -= 100
-            print g + str(100)
+            elif request >= 50:
+                request -= 50
+                print g + str(50)
 
-        elif request >= 50:
-            request -= 50
-            print g + str(50)
+            elif request >= 10:
+                request -= 10
+                print g + str(10)
 
-        elif request >= 10:
-            request -= 10
-            print g + str(10)
+            elif request >= 5:
+                request -= 5
+                print g + str(5)
 
-        elif request >= 5:
-            request -= 5
-            print g + str(5)
+            elif request >= 2:
+                request-= 2
+                print g + str(2)
 
-        elif request >= 2:
-            request-= 2
-            print g + str(2)
-
-        elif request >= 1:
-            request -= 1
-            print g + str(1)
+            elif request >= 1:
+                request -= 1
+                print g + str(1)
 atm(500, 277)
