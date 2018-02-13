4b88e79fb48f8b1976caf3e472fc64f6212f0619
0xThamer <thamertx0@gmail.com>
2018-02-13 23:32:24 +0300

    ok

---
+++ Forums/main.py
@@ -0,0 +1,15 @@
+from models import *
+
+user1 = Member("user1", 1)
+user2 = Member("user2", 2)
+post1 = Post("title1","body1")
+post2 = Post("title2","body2")
+post3 = Post("title3","body3")
+
+user_list = [user1,user2]
+post_list =[post1,post2,post3]
+
+for user in user_list:
+    print(user.info())
+    for post in post_list:
+        print(post.info())
--- Forums/models.py
+++ Forums/models.py
@@ -1,11 +1,26 @@
 class Member:
     def __init__(self, name , age):
+
         self.name = name
         self.age = age
 
 
+    def info(self):
+        distance(35)
+        print("Name:%s \nAge:%d " %(self.name , self.age))
+
 
 class Post:
-    def __init__(self, title , post):
+
+    def __init__(self,title , body):
         self.title = title
-        self.post = post
+        self.body = body
+
+
+    def info(self):
+        distance(34)
+        print("title: %s\nbody: %s" %(self.title, self.body))
+
+
+def distance(x):
+    print("-" * x)
