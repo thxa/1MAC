185e7cbd50001c7985ac3f8d4533c34de9d6a589
0xThamer <thamertx0@gmail.com>
2018-02-14 02:36:38 +0300

    thank you hichem2h

--- Forums/main.py
+++ Forums/main.py
@@ -1,10 +1,10 @@
-from models import *
+import models
 
-user1 = Member("user1", 1)
-user2 = Member("user2", 2)
-post1 = Post("title1","body1")
-post2 = Post("title2","body2")
-post3 = Post("title3","body3")
+user1 = models.Member("user1", 1)
+user2 = models.Member("user2", 2)
+post1 = models.Post("title1","body1")
+post2 = models.Post("title2","body2")
+post3 = models.Post("title3","body3")
 
 user_list = [user1,user2]
 post_list =[post1,post2,post3]
