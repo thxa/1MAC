be63353edf9bed61ffc9dcbaa742c94f29c9bb49
0xThamer <thamertx0@gmail.com>
2018-02-15 00:12:00 +0300

    thank you YaserAlnajjar

--- Forums/stores.py
+++ Forums/stores.py
@@ -2,10 +2,7 @@ class MemberStore():
     members = []
 
     def get_all(self):
-        if len(self.members) > 0:
-            return self.members
-        else:
-            return None
+        return self.members
 
     def add(self, member):
         self.members.append(member)
@@ -15,10 +12,7 @@ class PostStore(MemberStore):
     posts = []
 
     def get_all(self):
-        if len(self.posts) > 0:
-            return self.posts
-        else:
-            return None
+        return self.posts
 
     def add(self, post):
         self.posts.append(post)
