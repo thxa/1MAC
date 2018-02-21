e07bf4cccbb32220a229e81f4b10848dbb534d40
0xThamer <0xThamer@github.com>
2018-02-21 20:11:58 +0300

    add itertools

--- Forums/stores.py
+++ Forums/stores.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 class MemberStore:
     members = []
     last_id = 1
@@ -44,18 +47,16 @@ class MemberStore:
                 all_member[index] = member
 
     def get_members_with_posts(self, all_posts):
-        all_member = self.get_all()
-        for member in all_member:
-            for post in all_posts:
-                if member.id == post.member_id:
-                    member.posts.append(post)
-        return all_member
+        all_members = self.get_all()
+        for member, post in itertools.product(all_members, all_posts):
+            if post.member_id is member.id:
+                member.posts.append(post)
+        return all_members
 
     def get_top_two(self, all_posts):
-        all_posts_s = self.get_members_with_posts(all_posts)
-        all_members = self.get_all()
-        all_post = sorted(all_members, key=lambda top: len(top.posts), reverse=True)
-        return all_post[:2]
+        self.get_members_with_posts(all_posts)
+        store = sorted(self.get_all(), key=lambda top: len(top.posts), reverse=True)
+        return store[:2]
 
 
 class PostStore:
