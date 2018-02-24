98202e27bfab48780ee253e335ae529860432680
0xThamer <0xThamer@github.com>
2018-02-24 22:31:19 +0300

    fix inheritance

--- Forums/stores.py
+++ Forums/stores.py
@@ -45,43 +45,10 @@ class MemberStore(BaseStore):
     def __init__(self):
         super().__init__(MemberStore.members, MemberStore.last_id)
 
-    def get_all(self):
-        return self.members
-
-    def add(self, member):
-        member.id = self.last_id
-        self.members.append(member)
-        self.last_id += 1
-
-    def get_by_id(self, _id):
-        all_members = self.get_all()
-        result = None
-        for member in all_members:
-            if member.id == _id:
-                result = member
-                break
-
-        return result
-
-    def entity_exists(self, member):
-        # check if member exists or not
-        return member in self.get_all()
-
-    def delete(self, _id):
-        scan = self.get_by_id(_id)
-        self.members.remove(scan)
-
     def get_by_name(self, member_name):
         all_members = self.get_all()
         # algorithm for searching by name
         return (member for member in all_members if member.name == member_name)
-
-    def update(self, member):
-        all_member = self.get_all()
-        member_id = member.id
-        for index, e in enumerate(all_member):
-            if e.id == member_id:
-                all_member[index] = member
 
     def get_members_with_posts(self, all_posts):
         all_members = self.get_all()
@@ -104,37 +71,6 @@ class PostStore(BaseStore):
 
     def __init__(self):
         super().__init__(PostStore.posts, PostStore.last_id)
-
-    def get_all(self):
-        return self.posts
-
-    def add(self, post):
-        post.id = self.last_id
-        self.posts.append(post)
-        post.id += 1
-
-    def get_by_id(self, _id):
-        all_posts = self.get_all()
-        result = None
-        for post in all_posts:
-            if post.id == _id:
-                result = post
-                break
-        return result
-
-    def update(self, post):
-        all_post = self.get_all()
-        post_id = post.id
-        for index, e in enumerate(all_post):
-            if e.id == post_id:
-                all_post[index] = post
-
-    def delete(self, _id):
-        scan = self.get_by_id(_id)
-        self.posts.remove(scan)
-
-    def entity_exists(self, post):
-        return post in self.get_all()
 
     def get_posts_by_date(self):
         all_posts = self.get_all()
