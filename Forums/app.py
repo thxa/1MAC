cd4d49a93755383cc10422b9d441b0b3822710d3
0xThamer <0xThamer@github.com>
2018-02-27 01:31:46 +0300

    add index + css + js + img

--- Forums/app.py
+++ Forums/app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask("__name__")
 
+
 @app.route("/")
+def root():
+    return home()
+
+
 @app.route("/index")
 def home():
     return render_template("index.html")
